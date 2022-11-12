6380d170234cf2043ad39799bd1b58778403f570
D-Tretyakov <tretyakov.daniil.education@gmail.com>
2022-11-13 02:20:09 +0300

    created variable to store correct answer; now using lambda as event handler

--- GUI/ButtonPanel/ButtonPanel.cpp
+++ GUI/ButtonPanel/ButtonPanel.cpp
@@ -1,14 +1,35 @@
 #include "ButtonPanel.hpp"
+#include "MainFrame.hpp"
+#include <string>
 
 ButtonPanel::ButtonPanel(wxFrame* parent)
-    : wxPanel(parent, wxID_ANY)
+    : wxPanel(parent, wxID_ANY),
+      m_Parent(parent),
+      m_Correct(-1)
 {
     wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
-    for (auto& button : m_Buttons)
+    for (int i = 0; i < m_Buttons.size(); i++)
     {
-        button = new ChoiceButton(this, wxT("русская кнопка"));
-        vbox->Add(button, 0, wxALL | wxALIGN_CENTER_HORIZONTAL, 5);
+        m_Buttons[i] = new ChoiceButton(this, wxT("Обычный текст"));
+        vbox->Add(m_Buttons[i], 0, wxALL | wxALIGN_CENTER_HORIZONTAL, 5);
+
+        auto const handler = [i, this](wxCommandEvent& event){
+            OnButtonClick(event, i);
+        };
+        m_Buttons[i]->Bind(wxEVT_BUTTON, handler);
     }
 
+    m_Buttons[0]->SetLabel(wxT("текст 0"));
+    m_Buttons[1]->SetLabel(wxT("текст 1"));
+    m_Buttons[2]->SetLabel(wxT("текст 2"));
+    m_Buttons[3]->SetLabel(wxT("текст 3"));
+
     SetSizer(vbox);
+}
+
+void ButtonPanel::OnButtonClick(wxCommandEvent& event, int button_num)
+{
+    MainFrame* main_frame = dynamic_cast<MainFrame*>(m_Parent);
+    std::wstring text = std::wstring(L"кнопка #") + std::to_wstring(button_num);
+    main_frame->GetTranslationPanel()->SetText(text);
 }
--- GUI/ButtonPanel/ButtonPanel.hpp
+++ GUI/ButtonPanel/ButtonPanel.hpp
@@ -1,14 +1,20 @@
 #pragma once
 #include <wx/wx.h>
 #include "ChoiceButton/ChoiceButton.hpp"
+#include <vector>
+
 
 class ButtonPanel : public wxPanel
 {
 public:
     ButtonPanel(wxFrame* parent);
 
-    void OnButtonClick(wxCommandEvent& event);
+    void OnButtonClick(wxCommandEvent& event, int button_num);
+
+    void Refresh(std::vector<std::wstring> words, int correct);
 private:
     wxFrame* m_Parent;
-    ChoiceButton* m_Buttons[4];
+    std::vector<ChoiceButton*> m_Buttons;
+
+    int m_Correct; // index of button with correct answer
 };
